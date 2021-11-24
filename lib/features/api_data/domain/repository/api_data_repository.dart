b55b1b9ef67f54cabc41a01d7cf5fa5e5adc3336
Franco <francorossi@latechfactory.com>
2021-11-24 19:19:31 -0300

    more changes to init settings to api data

--- lib/features/api_data/domain/repository/api_data_repository.dart
+++ lib/features/api_data/domain/repository/api_data_repository.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:nubank_marketplace/core/failures.dart';
 import 'package:nubank_marketplace/features/api_data/domain/entities/api_data.dart';
 
-abstract class InitSettingsRepository {
+abstract class ApiDataRepository {
   Future<Either<Failure, ApiData>> getApiData();
 }
--- lib/features/api_data/domain/usecases/get_api_data.dart
+++ lib/features/api_data/domain/usecases/get_api_data.dart
@@ -3,10 +3,10 @@ import 'package:equatable/equatable.dart';
 import 'package:nubank_marketplace/core/failures.dart';
 import 'package:nubank_marketplace/core/usecases/usecase.dart';
 import 'package:nubank_marketplace/features/api_data/domain/entities/api_data.dart';
-import 'package:nubank_marketplace/features/api_data/domain/repository/init_settings_repository.dart';
+import 'package:nubank_marketplace/features/api_data/domain/repository/api_data_repository.dart';
 
 class GetApiData implements UseCase<ApiData, NoParams> {
-  final InitSettingsRepository repository;
+  final ApiDataRepository repository;
   GetApiData(this.repository);
   @override
   Future<Either<Failure, ApiData>> call(NoParams params) async {
