4e45f31232d7202fa2c065149ca383448bdbecb9
Franco <francorossi@latechfactory.com>
2021-11-25 13:28:42 -0300

    home and offers tiles integrated

---
+++ lib/features/offers/presentation/widgets/offer_tile.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:nubank_marketplace/features/offers/domain/entities/product_offer.dart';
+
+class OfferTile extends StatelessWidget {
+  final ProductOffer offer;
+
+  const OfferTile({Key key, this.offer}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Material(
+          elevation: 10,
+          shadowColor: Colors.grey[100],
+          borderRadius: BorderRadius.circular(8),
+          child: ExpansionTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: DecorationImage(
+                      image: NetworkImage(offer.product.imageUrl),
+                      fit: BoxFit.fill)),
+            ),
+            title: Text(offer.product.name),
+            subtitle: Text(offer.price.toString()),
+            childrenPadding: const EdgeInsets.all(20),
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(child: Text(offer.product.description))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: MaterialButton(
+                          onPressed: () {},
+                          textColor: Colors.white,
+                          textTheme: Theme.of(context).buttonTheme.textTheme,
+                          color: Theme.of(context).primaryColor,
+                          child: const Text(
+                            'Agregar',
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/pivots/home_page.dart
+++ lib/features/pivots/home_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nubank_marketplace/core/utils/custom_icons.dart';
+import 'package:nubank_marketplace/features/offers/presentation/bloc/offers_bloc.dart';
+import 'package:nubank_marketplace/features/offers/presentation/widgets/offer_tile.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = '/splasg-page';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          CustomIcons.nubank,
+          size: 40.0,
+          color: Colors.white,
+        ),
+        title: const Text('marketplace'),
+      ),
+      body: SingleChildScrollView(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BlocBuilder<OffersBloc, OffersState>(
+            builder: (context, offersState) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (offersState is OffersLoaded)
+                  ...offersState.offers.map((e) => OfferTile(
+                        offer: e,
+                      ))
+              ],
+            ),
+          ),
+        ),
+      )),
+    );
   }
 }
