3bcd5bc4a9069e623902b681ba3c099ec776b46a
Franco <francorossi@latechfactory.com>
2021-11-26 10:18:11 -0300

    network info test added

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,33 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nubank_marketplace/core/network/network_info.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('is connected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection',
+        () async {
+      //arrange
+      final tHasConnectionFuture = await Future.value(true);
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((realInvocation) async => tHasConnectionFuture);
+      //act
+      final result = await networkInfoImpl.isConnected;
+      //assert
+      verifyNever(mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+
+      verifyNoMoreInteractions(mockDataConnectionChecker);
+    });
+  });
+}
