574a3e9756931ff9344164edeb6c77542494c87c
Franco <francorossi@latechfactory.com>
2021-11-26 13:55:47 -0300

    launcher icons

--- lib/core/utils/token.dart
+++ lib/core/utils/token.dart
@@ -8,11 +8,17 @@ class TokenHelper {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
 
     if (sharedPreferences.containsKey(CACHED_TOKEN)) {
-      /*  if (JwtDecoder.isExpired(sharedPreferences.getString(CACHED_TOKEN))) {
+      bool tokenIsExpired =
+          JwtDecoder.isExpired(sharedPreferences.getString(CACHED_TOKEN)) ??
+              false;
+      if (tokenIsExpired) {
+        //This block is functional and its here because no expiraton date has been added to token, so tokenIsExpired will be false (because isExpired() will always throw null/error). Otherwise this block should have an endpoint call with a refresh token to update access token.
+        sharedPreferences.setString(CACHED_TOKEN,
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhd2Vzb21lY3VzdG9tZXJAZ21haWwuY29tIn0.cGT2KqtmT8KNIJhyww3T8fAzUsCD5_vxuHl5WbXtp8c");
         throw CacheExcepction(errorCode: 1);
-      } else { */
-      return sharedPreferences.getString(CACHED_TOKEN);
-      /* } */
+      } else {
+        return sharedPreferences.getString(CACHED_TOKEN);
+      }
     } else {
       throw CacheExcepction(errorCode: 0);
     }
